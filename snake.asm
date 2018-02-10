118e4f895d7f7fddc33564e0ea3bac8580f3f47f
Thiago Heron Avila <thiagoheronavila@gmail.com>
2018-02-10 19:23:06 -0200

    The border collision is working

--- snake.asm
+++ snake.asm
@@ -15,7 +15,13 @@
 #------------------------------------------------------------------------------#
 #			 Registers:					      	
 #  T0 - Count				
-#  T1 - Position where Snake Born(13060)				       
+#  T1 - Position where Snake Born(13060)
+#  T2 - Position temporary to calculate a collision	
+#  T3 - Pixel to calculate symbolic position, not is address: 512, -512, 4, -4
+#  T4 - Store valeus which 512, -512, 4, -4 to return the keypress function.
+#  T5 - Store values of border left or right
+#  T8 = 1
+#  T9 - SLTI			       
 #  20 - Black				       	
 #  21 - White				       				
 #  22 - Gray							       	
@@ -55,6 +61,7 @@ defineSettings:
 	add $10, $0, $9    # Pixels to map
 	lui $10, 0x1001
 	addi $t1, $10, 13060 
+	addi $t3, $0, 13060
 	jr $31
 	nop	
 
@@ -147,7 +154,10 @@ createSnake:
 	jr $31
 	nop
 #-----------------------------------------------------------------------------#
-#-----------------------------------------------------------------------------#		
+#-----------------------------------------------------------------------------#
+
+#-----------------------------------------------------------------------------#
+#	 Function to Control the Snake: W,A,S,D or 0 to Exit		      #		  		
 readKeyPress:	
 
 	li $v0, 12			# Read a Caracter: W,A,S,D or 0  to Exit
@@ -177,32 +187,136 @@ readKeyPress:
 	nop
 
 pressKeyW:
-	sw $22, 0($t1)	   # Print pixel to grey
-	addi $t1, $t1, -512# Position snake up
+	addi $t3, $t3, -512
+	addi $t4, $0, -512
+
+	j checkBorderCollision
+	nop	
+
+
+W:	sw $22, 0($t1)	   # Print pixel to grey
+	addi $t1, $t1, -512  # Position snake up
 	sw $21, 0($t1)	   # Print pixel to white
 	j readKeyPress
 	nop
 
 pressKeyS:
-	sw $22, 0($t1)	   # Print pixel to grey
-	addi $t1, $t1,  512# Position snake down
+	addi $t3, $t3, 512
+	addi $t4, $0, 512
+
+	j checkBorderCollision
+	nop
+
+S:	sw $22, 0($t1)	   # Print pixel to grey
+	addi $t1, $t1,  512 # Position snake down
 	sw $21, 0($t1)	   # Print pixel to white
 	j readKeyPress
 	nop
 
 pressKeyA:
-	sw $22, 0($t1)	   # Print pixel to grey
+	addi $t3, $t3, -4
+	addi $t4, $0, -4
+
+	j checkBorderCollision
+	nop
+
+A:	sw $22, 0($t1)	   # Print pixel to grey
 	addi $t1, $t1, -4  # Position snake up
 	sw $21, 0($t1)	   # Print pixel to white
 	j readKeyPress
 	nop
 
 pressKeyD:
-	sw $22, 0($t1)	   # Print pixel to grey
-	addi $t1, $t1, 4   # Position snake up
+	addi $t3, $t3, 4
+	addi $t4, $0, 4
+
+	j checkBorderCollision
+	nop
+
+D:	sw $22, 0($t1)	   # Print pixel to grey
+	addi $t1, $t1, 4  # Position snake up
 	sw $21, 0($t1)	   # Print pixel to white
 	j readKeyPress
 	nop
+
+
+#-----------------------------------------------------------------------------#
+#-----------------------------------------------------------------------------#
+
+#-----------------------------------------------------------------------------#
+#			      #	
+
+checkBorderCollision:
+	addi $t8, $0, 1
+	
+	# Check Border Top
+	slti $t9, $t3, 1541	# If previous position < 151 = Red Field, gameOver
+	beq $t9, $t8, gameOver
+	nop
+	
+	# Check Border Bottom
+	slti $t9, $t3, 31221	# If previous position < 31220 = Red Field, gamOver
+	beq $t9, $0, gameOver
+	nop
+	
+	# Check Border Left
+	addi $t5, $0, 1540
+	
+BL:	beq $t5, 31236, checkRight
+	nop
+		beq $t3, $t5, gameOver	# If t3 is equal to a one value of borderLeft
+		nop
+		addi $t5, $t5, 512
+		j BL
+		nop
+
+checkRight:
+	addi $t5, $0, 1528
+
+BR:	beq $t5, 31736, continue
+	nop
+		beq $t3, $t5, gameOver
+		nop
+		addi $t5, $t5, 512
+		j BR
+		nop
+
+continue:
+
+	beq $t4, -512, W
+	nop
+	beq $t4, 512, S
+	nop
+	beq $t4, 4, D
+	nop
+	beq $t4, -4, A
+	nop
+	
+	
+
+		
+
+
+gameOver:
+beq $t0, 32764, endgameOver
+	nop
+		sw $20, 0($10)
+		addi $10, $10, 4  # Address +=4;
+		addi $t0, $t0, 4  # Count +=4;
+		j gameOver
+		nop
+endgameOver:
+
+
+
+
+
+
+
+
+
+
+
 
 exit:
 	li $v0, 10
